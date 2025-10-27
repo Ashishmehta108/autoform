3a510ba6447554752db2f81cd5e13f906e9b9abe
Ashish mehta <ashishmehtawork108@gmail.com>
2025-10-28 02:36:33 +0530

    auth issues

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  console.log(req);
+  const token = await getToken({ req, secret: process.env. });
+  console.log(token );
   const url = req.nextUrl.clone();
 
   if (url.pathname.startsWith("/dashboard") && !token) {
